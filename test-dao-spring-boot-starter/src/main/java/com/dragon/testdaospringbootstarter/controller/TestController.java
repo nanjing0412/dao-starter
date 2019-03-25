687c1aacab285647ab4f52b1f25c561f23ac2936
hujw <hjw0535@163.com>
2019-03-25 17:58:25 +0800

    update

--- test-dao-spring-boot-starter/src/main/java/com/dragon/testdaospringbootstarter/controller/TestController.java
+++ test-dao-spring-boot-starter/src/main/java/com/dragon/testdaospringbootstarter/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.dragon.testdaospringbootstarter.controller;
 
+import com.dragon.testdaospringbootstarter.common.R;
 import com.dragon.testdaospringbootstarter.service.TestService;
 import com.github.jun.starter.dao.demo.HelloService;
+import com.github.jun.starter.dao.dsp.util.PageBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +19,7 @@ import java.util.Map;
  * @description:
  */
 @RestController
+@RequestMapping(value = "/test")
 public class TestController {
 
     @Autowired
@@ -31,6 +36,55 @@ public class TestController {
     @GetMapping("/test2")
     public List<Map> test2(){
         return testService.getBuyerList();
+    }
+
+    //========================
+    @RequestMapping(value = "/testPage")
+    public R testGetList() {
+        List<Map> list = testService.testGetList();
+        return R.ok().put("list", list);
+    }
+
+    @RequestMapping(value = "/testGetPageList")
+    public R testGetPageList() {
+        PageBean pageBean = testService.testGetPageList();
+        return R.ok().put("pageBean", pageBean);
+    }
+
+    @RequestMapping(value = "/testGetMap")
+    public R testGetMap() {
+        Map map = testService.testGetMap();
+        return R.ok().put("map", map);
+    }
+
+    @RequestMapping(value = "/testGetObject")
+    public R testGetObject() {
+        String str = testService.testGetObject();
+        return R.ok().put("str", str);
+    }
+
+    @RequestMapping(value = "/testUpdate")
+    public R testUpdate() {
+        testService.testUpdate();
+        return R.ok();
+    }
+
+    @RequestMapping(value = "/testBatchUpdate")
+    public R testBatchUpdate() {
+        testService.testBatchUpdate();
+        return R.ok();
+    }
+
+    @RequestMapping(value = "/testGetCount")
+    public R testGetCount() {
+        int count = testService.testGetCount();
+        return R.ok().put("count", count);
+    }
+
+    @RequestMapping(value = "/testExists")
+    public R testExists() {
+        boolean exists = testService.testExists();
+        return R.ok().put("exists", exists);
     }
 
 }
--- test-dao-spring-boot-starter/src/main/java/com/dragon/testdaospringbootstarter/service/TestService.java
+++ test-dao-spring-boot-starter/src/main/java/com/dragon/testdaospringbootstarter/service/TestService.java
@@ -1,9 +1,15 @@
 package com.dragon.testdaospringbootstarter.service;
 
+import cn.hutool.core.map.MapBuilder;
+import cn.hutool.core.map.MapUtil;
 import com.github.jun.starter.dao.dsp.bean.SqlDao;
+import com.github.jun.starter.dao.dsp.util.PageBean;
+import com.github.jun.starter.dao.dsp.util.PageParams;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +20,7 @@ import java.util.Map;
  * @description:
  */
 @Service
+@Slf4j
 public class TestService {
 
     @Autowired
@@ -23,5 +30,54 @@ public class TestService {
         return sqlDao.getList("test", "getBuyerList", new HashMap());
     }
 
+    //========================
+    public List<Map> testGetList() {
+        List<Map> list = sqlDao.getList("test", "testGetList", MapUtil.newHashMap());
+        return list;
+    }
+
+    public PageBean testGetPageList() {
+        PageBean pageBean = sqlDao.getPageList(
+                "test",
+                "testGetList",
+                MapUtil.newHashMap(),
+                PageParams.builder().pageNo(1).pageSize(10).build(),
+                false
+                );
+        return pageBean;
+    }
+
+    public Map testGetMap() {
+        Map map = sqlDao.getMap("test", "testGetList", MapUtil.of("username", "业务员"));
+        return map;
+    }
+
+    public String testGetObject() {
+        String str = sqlDao.getObject("test", "testGetObject", MapUtil.of("id", "1"), String.class);
+        return str;
+    }
+
+    public void testUpdate() {
+        int updateNum = sqlDao.update("test", "testUpdate",
+                new MapBuilder(MapUtil.newHashMap()).put("id", "1").put("username", "超级管理员2").build());
+        log.info("updateNum = {}", updateNum);
+    }
+
+    public void testBatchUpdate() {
+        List<Map<String, Object>> whereList = new ArrayList<>();
+        whereList.add(new MapBuilder(MapUtil.newHashMap()).put("id", "1").put("username", "超级管理员2").build());
+        whereList.add(new MapBuilder(MapUtil.newHashMap()).put("id", "2").put("username", "业务员2").build());
+        int[] updateResult = sqlDao.batchUpdate("test", "testUpdate", whereList);
+        log.info("updateResult = {}", updateResult);
+    }
+
+    public int testGetCount() {
+        int count = sqlDao.getCount("test", "testGetList", MapUtil.newHashMap());
+        return count;
+    }
+
+    public boolean testExists() {
+        return sqlDao.exists("test", "testGetList", MapUtil.of("username", "业务员"));
+    }
 
 }
