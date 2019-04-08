5094b42f286042ca4f966bf903eeb8cefce0ff04
hujw <hjw0535@163.com>
2019-04-08 15:57:44 +0800

    数据源优先支持druid

--- dao-spring-boot-starter-autoconfigurer/src/main/java/com/github/jun/starter/dao/dsp/jdbc/DataSourceConfig.java
+++ dao-spring-boot-starter-autoconfigurer/src/main/java/com/github/jun/starter/dao/dsp/jdbc/DataSourceConfig.java
@@ -1,5 +1,7 @@
 package com.github.jun.starter.dao.dsp.jdbc;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -20,12 +22,20 @@ import java.util.Map;
 @Configuration
 public class DataSourceConfig {
 
+    @Value("${spring.datasource.type}")
+    private String datasourceType;
+
     public static Map dataSourceMap = new HashMap();
 
     @Bean
     @ConfigurationProperties("spring.datasource")
     public DataSource mysqlDataSource() {
-        DataSource mysqlDataSource = DataSourceBuilder.create().build();
+        DataSource mysqlDataSource;
+        if("com.alibaba.druid.pool.DruidDataSource".equals(datasourceType)) {
+            mysqlDataSource = new DruidDataSource();
+        }else{
+            mysqlDataSource = DataSourceBuilder.create().build();
+        }
         dataSourceMap.put("mysql", mysqlDataSource);
         return mysqlDataSource;
     }
